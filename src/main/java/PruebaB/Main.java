d64fe27255e252ce48df20197e640a6fd4b4931d
TomiCavaco <tomascavcoagt@gmail.com>
2025-04-08 14:14:08 +0200

    mensaje

--- src/main/java/PruebaB/Main.java
+++ src/main/java/PruebaB/Main.java
@@ -6,22 +6,22 @@ public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        Scanner lector = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el importe sin impuestos:");
-        double importeInicial = lector.nextDouble();
+        double importeInicial = sc.nextDouble();
         double resultadoFinal = 0;
 
         System.out.println("Especifique la categoría de IVA (general, reducido o superreducido):");
-        String categoriaIVA = lector.next();
+        String categoriaIVA = sc.next();
 
         System.out.println("Indique el tipo de descuento (nopro, mitad, meno5 o 5porc):");
-        String descuentoAplicado = lector.next();
+        String descuentoAplicado = sc.next();
 
    
         double valorIVA = Iva.calcularIVA(importeInicial, categoriaIVA);
         double importeConIVA = importeInicial + valorIVA;
         double importeFinal = Iva.aplicarPromocion(importeConIVA, descuentoAplicado);
 
-        lector.close();
+        
     }
 }
